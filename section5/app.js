d539b07349d90f4cdd4502bd6147a0889a300c37
Lionel Herrero Battista <lionherrero@gmail.com>
2021-02-15 19:43:55 -0300

    section 5

--- section5/app.js
+++ section5/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -10,17 +11,25 @@ const app = express();
     next()
 });*/
 
+//Registra un middleware, parsea el body del html y hace un next();
+app.use(bodyParser.urlencoded());
+
 //Se puede definir el path como primer parámetro
 //El orden importa!! De lo particular a lo general
 app.use('/add-product', (req, res, next) => {
-    console.log('In another middleware!');
-    res.send('<h1>The "Add Product" Page</h1>');
+    //Creo un from para enviar el producto
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
     //Cuando se manda una res, no va next()!!
+});
+
+app.post('/product', (req, res, next) => {
+    //Recibe el body parseado como objeto javascript
+    console.log(req.body);
+    res.redirect('/');
 });
 
 //Último, porque toma todo lo que empiece con '/'
 app.use('/', (req, res, next) => {
-    console.log('In another middleware!');
     res.send('<h1>Hello form Express!</h1>')
 });
 
